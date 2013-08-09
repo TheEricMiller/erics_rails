ad40fc9f0bbdf2315015b2804cf7c9ee19b29b01
Eric Miller <Eric@TheEricMiller.com>
2013-08-09 02:28:29 -0400

    created new Ruby on Rails4 app ~~~~~~~~~~~~~~~~~~~~~~~~~~~ $ rails new rails_app_name $ cd rails_app_name $ rvm --create --ruby-version use ruby-2.0@rails_app_name $ bundle $ echo -e "\n# Ignore db config \n/config/database.yml" >> .gitignore $ echo THE-TO-DO-LIST: >> ~LIST.todo $ subl . ~~~~~~~~~ $ git init $ git flow init $ git flow feature start Setup
    > update README
    
    $ git commit -a -m "created new Ruby on Rails4 app"
    ~~~~~~~~~~~~~~~~~~~~~~~~~~~
    ### Update Rails Version ?
    $ rvm get stable
    $ rvm list known
    $ rvm install 2.0
    $ rvm use 2.0
    $ rvm gemset use rails4 --create
    $ gem install rails
    $ rvm list
    $ ruby -v
    $ rails -v
    ~~~~~~~~~~~~~~~~~~~~~~~~~~~

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EricsRails::Application.config.secret_key_base = '2d09734747c442e46fdd310e1c44f400ccb4410cb57f45e0b47052f758b563bf89a2d023897547959c9f60a94d4a1e34ee141db522e18ae34d4abea1e75915bf'
