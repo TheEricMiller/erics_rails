045c364334a82269f508972b16c44486d7a00b9d
Eric Miller <Eric@TheEricMiller.com>
2013-08-09 04:22:54 -0400

    generated 'welcome' controller ~~~~~~~~~ $ rails g controller welcome index > config/routes.rb ~ root :to => 'welcome#index'

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 EricsRails::Application.routes.draw do
+  get "welcome/index"
+
+  root 'welcome#index'
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +58,3 @@ EricsRails::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
